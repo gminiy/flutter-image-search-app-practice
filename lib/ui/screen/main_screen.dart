12173c075f0f9c2abeba46833435830836eefd6d
gminiy <gmini.y@gmail.com>
2023-12-28 22:33:02 +0900

    feat: main screen ui

--- lib/ui/screen/main_screen.dart
+++ lib/ui/screen/main_screen.dart
@@ -10,6 +10,55 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('hi'));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF4FB6B2),
+                      width: 2,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF4FB6B2),
+                      width: 2,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF4FB6B2),
+                      width: 2,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Text('$index');
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
