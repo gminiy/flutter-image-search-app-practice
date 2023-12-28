cd0e3bc01fbe3f9ed7746376ecc008a3842c6104
gminiy <gmini.y@gmail.com>
2023-12-28 20:00:15 +0900

    project init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// url: "https://pixabay.com/api/?key=41485709-10bae150ab77c646c4aade16f&image_type=photo"
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
