982b8f2e401178737537f2203353b7bd0a6f0d05
gminiy <gmini.y@gmail.com>
2024-01-06 13:19:58 +0900

    feat: image_model

---
+++ lib/domain/model/image_model.dart
@@ -0,0 +1,45 @@
+class ImageModel {
+  final String _url;
+
+//<editor-fold desc="Data Methods">
+  const ImageModel({
+    required String url,
+  }) : _url = url;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is ImageModel &&
+          runtimeType == other.runtimeType &&
+          _url == other._url);
+
+  @override
+  int get hashCode => _url.hashCode;
+
+  @override
+  String toString() {
+    return 'ImageModel{' + ' _url: $_url,' + '}';
+  }
+
+  ImageModel copyWith({
+    String? url,
+  }) {
+    return ImageModel(
+      url: url ?? this._url,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_url': this._url,
+    };
+  }
+
+  factory ImageModel.fromJson(Map<String, dynamic> map) {
+    return ImageModel(
+      url: map['_url'] as String,
+    );
+  }
+
+//</editor-fold>
+}
