df8eb5c5bb1ec9f5a87ca4f165892f6876f46ff6
gminiy <gmini.y@gmail.com>
2024-01-06 13:37:15 +0900

    feat: pixabay_api.dart

---
+++ lib/data/data_source/remote/pixabay_api.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:image_search_app_practice/data/dto/pixabay_image_query_dto.dart';
+import 'package:http/http.dart' as http;
+class PixabayApi {
+  final String _baseUrl = 'https://pixabay.com/api/?key=41485709-10bae150ab77c646c4aade16f&image_type=photo';
+
+  Future<PixabayImageQueryDto> queryImage(String? query) async {
+    final String url = query == null ? _baseUrl : '$_baseUrl&q=$query';
+    final http.Response response = await http.get(Uri.parse(url));
+
+    return PixabayImageQueryDto.fromJson(jsonDecode(response.body));
+  }
+}
