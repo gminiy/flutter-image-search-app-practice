2e17b7bb35c13f4ecca147ae634727cf02b39539
gminiy <gmini.y@gmail.com>
2024-01-07 17:02:56 +0900

    fix: viewModel extends changeNotifier

--- lib/presentation/view_model/main_view_model.dart
+++ lib/presentation/view_model/main_view_model.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:image_search_app_practice/domain/repository/image_repository.dart';
 import 'package:image_search_app_practice/presentation/view_model/main_state.dart';
 
-class MainViewModel {
+class MainViewModel extends ChangeNotifier {
   final ImageRepository imageRepository;
   final _state = const MainState();
 
   get state => _state;
 
-  const MainViewModel({
+  MainViewModel({
     required this.imageRepository,
   });
 }
